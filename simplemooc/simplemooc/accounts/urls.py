b30ca0b0187fa706ed73e5b55d06bb92f7732276
Mayron Ceccon <mayron.ceccon@gmail.com>
2018-01-27 16:25:12 -0200

    inserir criação de usuário e logout

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
+from simplemooc.accounts.views import register
 
 urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^register/$', login, {'template_name': 'accounts/login.html'}, name='register'),
+    url(r'^sair/$', logout, {'next_page': 'core:home'}, name='logout'),
+    url(r'^cadastre-se/$', register, name='register'),
 ]
