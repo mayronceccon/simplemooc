eed095798692690241816a022a2345f0c85f0b94
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-06-14 11:28:50 -0300

    Remover projeto com versão anterior

--- simplemooc/simplemooc/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-
-from simplemooc.core.views import home, contact
-
-urlpatterns = [
-    url(r'^$', home, name='home'),
-    url(r'^contato/$', contact, name='contact'),
-]
