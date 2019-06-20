0bc9b71bda914f84b7f023d8e2b867a57bbcdd33
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-06-20 18:38:17 -0300

    9. Página de Contato

--- main/views.py
+++ main/views.py
@@ -5,8 +5,12 @@ from django.http import HttpResponse
 def home(request):
     return render(
         request, 
-        'home.html', 
-        {
-            'usuario': 'Mayron'
-        }
+        'home.html',
+    )
+
+
+def contact(request):
+    return render(
+        request,
+        'contact.html',
     )
